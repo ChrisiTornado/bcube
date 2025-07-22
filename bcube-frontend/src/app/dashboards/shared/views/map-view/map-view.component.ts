4472abaf8e2cda4beafc9a611eb3525a23b54c7a
Christophe Andunda <52066089+ChrisiTornado@users.noreply.github.com>
2025-07-22 23:16:12 +0200

    marker added

--- bcube-frontend/src/app/dashboards/shared/views/map-view/map-view.component.ts
+++ bcube-frontend/src/app/dashboards/shared/views/map-view/map-view.component.ts
@@ -1,28 +1,56 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from '../../../../../environments/environment';
+import { StudioService } from '../../../../services/studio.service';
+import { studio } from '../../../../models/studio';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { LoadingSpinnerComponent } from '../../../../shared/loading-spinner/loading-spinner.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-map-view',
   standalone: true,
-  imports: [],
+  imports: [LoadingSpinnerComponent, CommonModule],
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.css'
 })
-export class MapViewComponent implements AfterViewInit {
+export class MapViewComponent implements OnInit {
+  studios$!: Observable<studio[]>;
+  loading$ = this.studioService.loading$;
+  selectedStudio: studio | null = null;
 
-  ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/chrisitornado/cmd60zbgu00u901qoevjtel7q',
-      center: [16.3738, 48.2082],
-      zoom: 12,
-      accessToken: environment.token
+  constructor(private studioService: StudioService) {}
+
+  ngOnInit(): void {
+    this.studios$ = this.studioService.studios$;
+
+    this.studios$.pipe(take(1)).subscribe(studios => {
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/chrisitornado/cmd60zbgu00u901qoevjtel7q',
+        center: [16.3738, 48.2082],
+        zoom: 12,
+        accessToken: environment.token,
+        attributionControl: false
+      });
+
+      map.addControl(new mapboxgl.AttributionControl({
+        compact: true,
+        customAttribution: '© Mapbox © OpenStreetMap'
+      }), 'bottom-right');
+
+      studios.forEach(studio => {
+        if (studio.longitude != null && studio.latitude != null) {
+          const marker = new mapboxgl.Marker()
+            .setLngLat([studio.longitude, studio.latitude])
+            .addTo(map);
+
+          marker.getElement().addEventListener('click', () => {
+            this.selectedStudio = studio;
+          });
+        }
+      });
     });
-
-    // Beispiel-Pin hinzufügen
-    new mapboxgl.Marker()
-      .setLngLat([16.3738, 48.2082])
-      .addTo(map);
   }
 }
