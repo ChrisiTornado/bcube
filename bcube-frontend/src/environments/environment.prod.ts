95ee726ad2b6ab9848e41b102d5603081c15fa6a
candunda <christophe.andunda@gmail.com>
2026-04-10 16:40:28 +0200

    add prod env

---
+++ bcube-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  adminApiUrl: 'http://user-service:8080/api/admin',
+  authUrl: 'http://user-service:8080/api/auth',
+  bookingApiUrl: 'http://booking-service:8082/api',
+  studioApiUrl: 'http://studio-service:8081/api',
+  accessServiceApiUrl: 'http://access-permissions-service:8083/api/access',
+  token: 'YOUR_MAPBOX_TOKEN'
+};
