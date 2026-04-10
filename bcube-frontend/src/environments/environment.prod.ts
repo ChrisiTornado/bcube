f1f9829cca8aee5602db4c14cbc3bd0e1f2d7faa
candunda <christophe.andunda@gmail.com>
2026-04-10 17:15:11 +0200

    added user URL to env

--- bcube-frontend/src/environments/environment.prod.ts
+++ bcube-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   adminApiUrl: 'http://user-service:8080/api/admin',
+  userApiUrl: 'http://user-service:8080/api/users',
   authUrl: 'http://user-service:8080/api/auth',
   bookingApiUrl: 'http://booking-service:8082/api',
   studioApiUrl: 'http://studio-service:8081/api',
